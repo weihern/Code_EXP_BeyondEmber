d5c16a4888490cf72185d2f337fc1093215752e2
Teo Wei Hern <wei-hern@hotmail.com>
2023-06-17 23:34:08 +0800

    Home page data provided

--- pages/home.js
+++ pages/home.js
@@ -1,57 +1,126 @@
 import LoadStyles from "../assets/stylesheets/main-style";
 import CustomButton from "../components/button";
-import { Button, StyleSheet, Text, View } from 'react-native';
-import * as React from 'react';
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as React from "react";
 import ChallengeStack1 from "./junwei";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useState } from "react";
 
-const HomeMain = ({navigation}) => {
-    const [MainStyles, setStyles] = React.useState(null);
+import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../components/firebase";
 
-    React.useEffect(() => {
-        const loadStyles = async () => {
-          const loadedStyles = await LoadStyles();
-          setStyles(loadedStyles);
-        };
-    
-        loadStyles();
-    }, []);
+const HomeMain = ({ navigation }) => {
+  const [MainStyles, setStyles] = React.useState(null);
 
-    function change(id){
-        console.log("change page");
-        navigation.navigate('Profile');
-        if(id==="jw"){
-          navigation.navigate('ChallengeStack1');
-        }else if(id==="wh"){
-          //weihern put the name of navigator screen here
-          navigation.navigate('ChallengeStack1');
-        }else if(id==="b"){
-          //brian put the name of navigator screen here
-          navigation.navigate('ChallengeStack1');
-        }
+  React.useEffect(() => {
+    const loadStyles = async () => {
+      const loadedStyles = await LoadStyles();
+      setStyles(loadedStyles);
+    };
+
+    loadStyles();
+  }, []);
+
+  const [profile, setProfile] = useState([]);
+  
+  // get user email from token
+  const email = "test@hotmail.com";
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "users"), where("__name__", "==", email)),
+      (snapshot) => {
+        const documents = snapshot.docs.reduce((acc, doc) => {
+          acc = {
+            ...doc.data(),
+          };
+          return acc;
+        }, {});
+        // console.log(documents);
+        setProfile(documents);
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "challenges"), (snapshot) => {
+      const documents = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      // console.log(documents);
+      setNotes(documents);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  console.log(notes)
+  console.log(profile);
+  const projects = [
+    "SGX Marketing Project",
+    "DBS Data Analytic Project",
+    "Ember Mobile App Project",
+  ];
+  console.log(projects);
+  
+  function change(id) {
+    console.log("change page");
+    navigation.navigate("Profile");
+    if (id === "jw") {
+      navigation.navigate("ChallengeStack1");
+    } else if (id === "wh") {
+      //weihern put the name of navigator screen here
+      navigation.navigate("ChallengeStack1");
+    } else if (id === "b") {
+      //brian put the name of navigator screen here
+      navigation.navigate("ChallengeStack1");
     }
+  }
 
-    return (
-        <>
-     {  MainStyles && <View style={[MainStyles.bg,MainStyles.container]}>
-            {/* <Text style={MainStyles.textPrimary}>Open up App.js to start working on your app!</Text> */}
-            {/* <View style={MainStyles.btnAction}><Button title="Challenge" onPress={() => {}}/></View>
-            */}
-            {/* <CustomButton type="action" text="Challenge"/>
+  return (
+    <>
+      {MainStyles && (
+        <View style={[MainStyles.bg, MainStyles.container]}>
+          {/* <Text style={MainStyles.textPrimary}>Open up App.js to start working on your app!</Text> */}
+          {/* <View style={MainStyles.btnAction}><Button title="Challenge" onPress={() => {}}/></View>
+           */}
+          {/* <CustomButton type="action" text="Challenge"/>
             <CustomButton
             text="Submit"
             divStyle={MainStyles.btnPrimary}
             textStyle={MainStyles.btnPrimaryText} 
             onPress={change}
             /> */}
-            <Button title="Jun Wei" onPress={(e)=>{change("jw")}}/>
-            <Button title="Wei Hern" onPress={(e)=>{change("wh")}}/>
-            <Button title="Brian" onPress={(e)=>{change("b")}}/>
-            <Button title="Hui Ru" onPress={(e)=>{change("hr")}}/>
-        </View>}
-      </>
-    );
-}
+          <Button
+            title="Jun Wei"
+            onPress={(e) => {
+              change("jw");
+            }}
+          />
+          <Button
+            title="Wei Hern"
+            onPress={(e) => {
+              change("wh");
+            }}
+          />
+          <Button
+            title="Brian"
+            onPress={(e) => {
+              change("b");
+            }}
+          />
+          <Button
+            title="Hui Ru"
+            onPress={(e) => {
+              change("hr");
+            }}
+          />
+        </View>
+      )}
+    </>
+  );
+};
 
 const Stack = createNativeStackNavigator();
 export default function Home() {
--- pages/profile.js
+++ pages/profile.js
@@ -1,40 +1,32 @@
 import LoadStyles from "../assets/stylesheets/main-style";
 import CustomButton from "../components/button";
-import { StyleSheet, Text, View, PixelRatio } from 'react-native';
-import * as React from 'react';
+import { StyleSheet, Text, View, PixelRatio } from "react-native";
+import * as React from "react";
 import { useEffect, useState } from "react";
-// import 
-import {
-  collection,
-  doc,
-  onSnapshot,
-  query,
-  where
-} from "firebase/firestore";
+// import
+import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../components/firebase";
 
-import rewardAsObjects from "../data/rewards.js";
+const Profile = ({ navigation }) => {
+  const remToDp = (rem) => rem * PixelRatio.get();
 
-const Profile = ({navigation}) => {
-    const remToDp = (rem) => rem * PixelRatio.get();
+  const [MainStyles, setStyles] = useState(null);
 
-    const [MainStyles, setStyles] = useState(null);
+  useEffect(() => {
+    const loadStyles = async () => {
+      const loadedStyles = await LoadStyles();
+      setStyles(loadedStyles);
+    };
 
-    useEffect(() => {
-        const loadStyles = async () => {
-          const loadedStyles = await LoadStyles();
-          setStyles(loadedStyles);
-        };
-    
-        loadStyles();
-    }, []);
+    loadStyles();
+  }, []);
 
-    const [profile, setProfile] = useState([]);
-    // get user email from token
-    const email = "test@hotmail.com"
-    useEffect(() => {
-      const unsubscribe = onSnapshot(
-        query(collection(db, "users"), where("__name__", "==", email)),
+  const [profile, setProfile] = useState([]);
+  // get user email from token
+  const email = "test@hotmail.com";
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "users"), where("__name__", "==", email)),
       (snapshot) => {
         const documents = snapshot.docs.reduce((acc, doc) => {
           acc = {
@@ -44,28 +36,35 @@ const Profile = ({navigation}) => {
         }, {});
         // console.log(documents);
         setProfile(documents);
-      });
-      return () => unsubscribe();
-    }, []);
-    console.log(profile)
-    console.log(rewardAsObjects())
-    return (
-        <>
-        {MainStyles && 
+      }
+    );
+    return () => unsubscribe();
+  }, []);
+  console.log(profile);
+  const rewards = ["grab", "ntuc", "capitalLand", "gojek"];
+  console.log(rewards);
+  return (
+    <>
+      {MainStyles && (
         <View style={[MainStyles.bg, MainStyles.container]}>
           <Text style={MainStyles.textPrimary}>This is Profile Page</Text>
           {/* <View style={MainStyles.btnAction}><Button title="Challenge" onPress={() => {}}/></View>
            */}
-           <CustomButton type="action" text="Challenge"/>
-           <CustomButton 
-           text="Submit"
-           divStyle={{backgroundColor:"#BB86FC",borderRadius:10, padding:remToDp(2), width:'auto'}}
-           textStyle={{color:"#FFFFFF", fontSize:18}} 
-           />
-        </View>}
-        </>
-      );
-
-}
+          <CustomButton type="action" text="Challenge" />
+          <CustomButton
+            text="Submit"
+            divStyle={{
+              backgroundColor: "#BB86FC",
+              borderRadius: 10,
+              padding: remToDp(2),
+              width: "auto",
+            }}
+            textStyle={{ color: "#FFFFFF", fontSize: 18 }}
+          />
+        </View>
+      )}
+    </>
+  );
+};
 
 export default Profile;
