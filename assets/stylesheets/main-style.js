baa935f8532e9ffe06d36d6f856999e72b43eb31
speedyfish <brianlimjj5@gmail.com>
2023-06-18 03:52:09 +0800

    added profile page

--- assets/stylesheets/main-style.js
+++ assets/stylesheets/main-style.js
@@ -1,5 +1,5 @@
 import { StyleSheet, PixelRatio } from "react-native";
-import * as Font from 'expo-font';
+import * as Font from "expo-font";
 
 /*colors*/
 const mainPurple = "#BB86FC";
@@ -16,169 +16,188 @@ const lightGrey = "#D9D9D9";
 const orange = "#FFA184";
 const yellow = "#FDFF84";
 const levelBar = "#FFE500";
-const textLight =  "#FFFFFF";
+const textLight = "#FFFFFF";
 const textDark = "#272A37";
 const textGrey = "#BDBDBD";
 
 const remToDp = (rem) => rem * PixelRatio.get();
 
 const loadFonts = async () => {
-    await Font.loadAsync({
-      'righteous': require('../fonts/Righteous/Righteous-Regular.ttf'),
-      'alegraya sans reg': require('../fonts/Alegreya_Sans/AlegreyaSans-Regular.ttf')
-    });
+  await Font.loadAsync({
+    righteous: require("../fonts/Righteous/Righteous-Regular.ttf"),
+    "alegraya sans reg": require("../fonts/Alegreya_Sans/AlegreyaSans-Regular.ttf"),
+  });
 };
-  
+
 loadFonts();
 
-const LoadStyles = async() => {
-    await loadFonts();
-    return StyleSheet.create({
-        headerDiv:{
-            backgroundColor: mainDark,
-            justifyContent:'start',
-            alignItems:'center',
-            width:'100%',
-            paddingVertical: remToDp(3),
-            paddingHorizontal: remToDp(5),
-            flexDirection: 'row'
-        },
-    
-        header:{
-            fontFamily:'righteous',
-            color:'#FFFFFF',
-            fontSize: 18,
-            marginRight:'auto'
-        },
-    
-        container: {
-            width:"100%",
-            height:"100%",
-            justifyContent:'center',
-            alignItems:'center',
-            display:'flex',
-            backgroundColor: mainDark
-        },
+const LoadStyles = async () => {
+  await loadFonts();
+  return StyleSheet.create({
+    headerDiv: {
+      backgroundColor: mainDark,
+      justifyContent: "start",
+      alignItems: "center",
+      width: "100%",
+      paddingVertical: remToDp(3),
+      paddingHorizontal: remToDp(5),
+      flexDirection: "row",
+    },
 
-        // shadowDiv:{
-        //     ...Platform.select({
-        //         ios: {
-        //           shadowColor: 'black',
-        //           shadowOpacity: 0.5,
-        //           shadowOffset: { width: 0, height: 2 },
-        //           shadowRadius: 4,
-        //         },
-        //         android: {
-        //           elevation: 4,
-        //         },
-        //       }),
-        // },
-        shadowDiv:{
-            shadowColor: 'black',
-            shadowOpacity: 0.5,
-            shadowOffset: { width: 0, height: 2 },
-            shadowRadius: 4,
-            elevation: 4
-        },
-        
-        bg: {
-            backgroundColor: mainDark
-        },
-    
-        containerPrimary:{
-            backgroundColor: divColor,
-            borderRadius: 10,
-            width: "100%",
-            display: 'flex',
-            flexDirection: 'column',
-            marginVertical: 5
-        },
+    header: {
+      fontFamily: "righteous",
+      color: "#FFFFFF",
+      fontSize: 18,
+      marginRight: "auto",
+    },
 
-        rowDiv:{
-            display: 'flex',
-            flexDirection: 'row',
-            width: '100%',
-            paddingHorizontal: remToDp(3),
-            paddingVertical: remToDp(3)
-        },
+    container: {
+      width: "100%",
+      height: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+      display: "flex",
+      backgroundColor: mainDark,
+    },
 
-        colDiv:{
-            display: 'flex',
-            flexDirection:'column',
-            width:'100%',
-            padding: 5
-        },
+    // shadowDiv:{
+    //     ...Platform.select({
+    //         ios: {
+    //           shadowColor: 'black',
+    //           shadowOpacity: 0.5,
+    //           shadowOffset: { width: 0, height: 2 },
+    //           shadowRadius: 4,
+    //         },
+    //         android: {
+    //           elevation: 4,
+    //         },
+    //       }),
+    // },
+    shadowDiv: {
+      shadowColor: "black",
+      shadowOpacity: 0.5,
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 4,
+      elevation: 4,
+    },
 
-        hardDiv:{
-            backgroundColor: hard
-        },
+    bg: {
+      backgroundColor: mainDark,
+    },
 
-        moderateDiv:{
-            backgroundColor: moderate
-        },
+    containerPrimary: {
+      backgroundColor: divColor,
+      borderRadius: 10,
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      marginVertical: 5,
+    },
 
-        easyDiv:{
-            backgroundColor: easy
-        },
-    
-        btnPrimary: {
-            backgroundColor: mainPurple,
-            borderRadius:10, 
-            padding:remToDp(2), 
-            width:'auto'
-        },
+    rowDiv: {
+      display: "flex",
+      flexDirection: "row",
+      width: "100%",
+      paddingHorizontal: remToDp(3),
+      paddingVertical: remToDp(3),
+    },
 
-        btnPrimaryText:{
-            color: "#FFFFFF",
-            fontSize: 18
-        },
-    
-        //default btn style
-        btnDfText: {
-            fontFamily: 'righteous',
-            fontSize: 18,
-            color: "#FFFFFF"
-        },
-    
-        btnDf: {
-            padding: remToDp(0.5)
-        },
-    
-        //call to action btn style
-        btnAction: {
-            backgroundColor: pink,
-            borderRadius: 20,
-            height: 55,
-            width: 220,
-            alignItems:'center',
-            justifyContent:'center'
-        },
-    
-        btnActionText:{
-            fontSize: 30,
-            color: textLight
-        },
-    
-        icon: {
-            fill: inactive
-        },
-    
-        iconActive: {
-            fill: mainPurple
-        },
-    
-        textPrimary :{
-            color: textLight,
-            fontFamily: 'alegraya sans reg'
-        },
+    colDiv: {
+      display: "flex",
+      flexDirection: "column",
+      width: "100%",
+      padding: 5,
+    },
 
-        textHeader: {
-            color: textLight,
-            fontFamily: 'righteous',
-            fontSize: 24
-        }
-    });
-}
+    hardDiv: {
+      backgroundColor: hard,
+    },
+
+    moderateDiv: {
+      backgroundColor: moderate,
+    },
+
+    easyDiv: {
+      backgroundColor: easy,
+    },
+
+    btnPrimary: {
+      backgroundColor: mainPurple,
+      borderRadius: 10,
+      padding: remToDp(2),
+      width: "auto",
+    },
+
+    btnPrimaryText: {
+      color: "#FFFFFF",
+      fontSize: 18,
+    },
+
+    //default btn style
+    btnDfText: {
+      fontFamily: "righteous",
+      fontSize: 18,
+      color: "#FFFFFF",
+    },
+
+    btnDf: {
+      padding: remToDp(0.5),
+    },
+
+    //call to action btn style
+    btnAction: {
+      backgroundColor: pink,
+      borderRadius: 20,
+      height: 55,
+      width: 220,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+
+    btnActionText: {
+      fontSize: 30,
+      color: textLight,
+    },
+
+    icon: {
+      fill: inactive,
+    },
+
+    iconActive: {
+      fill: mainPurple,
+    },
+
+    textPrimary: {
+      color: textLight,
+      fontFamily: "alegraya sans reg",
+    },
+
+    textHeader: {
+      color: textLight,
+      fontFamily: "righteous",
+      fontSize: 24,
+    },
+    icon: {
+      fill: inactive,
+    },
+    iconActive: {
+      fill: mainPurple,
+    },
+    textPrimary: {
+      color: textLight,
+      fontFamily: "alegraya sans reg",
+    },
+    textHeader: {
+      color: textLight,
+      fontFamily: "righteous",
+      fontSize: 24,
+    },
+    colors: {
+      bargraphColor: "#FFE500",
+      bargraphTextColor: "#FF0000",
+    },
+  });
+};
 
 // const MainStyles = StyleSheet.create({
 //     headerDiv:{
@@ -204,7 +223,7 @@ const LoadStyles = async() => {
 //         justifyContent:'center',
 //         alignItems:'center'
 //     },
-    
+
 //     bg: {
 //         backgroundColor: mainDark
 //     },
--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,28 @@
 import LoadStyles from "../assets/stylesheets/main-style";
 import CustomButton from "../components/button";
-import { StyleSheet, Text, View, PixelRatio } from "react-native";
+import { StyleSheet, Text, View, PixelRatio, Image } from "react-native";
 import * as React from "react";
 import { useEffect, useState } from "react";
 // import
 import { collection, doc, onSnapshot, query, where } from "firebase/firestore";
 import { db } from "../components/firebase";
+import * as Progress from "react-native-progress";
+import { Dimensions } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import ProfileStatsCard from "../components/ProfileStatsCard";
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from "react-native-chart-kit";
+
+const deviceWidth = Dimensions.get("window").width;
+const barWidth = deviceWidth * 0.7;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const Profile = ({ navigation }) => {
   const remToDp = (rem) => rem * PixelRatio.get();
@@ -43,28 +60,174 @@ const Profile = ({ navigation }) => {
   console.log(profile);
   const rewards = ["grab", "ntuc", "capitalLand", "gojek"];
   console.log(rewards);
+
+  const data = {
+    labels: ["Innovation", "Teamwork", "Leadership", "Professional"],
+    datasets: [
+      {
+        data: [
+          profile.stats?.innovative,
+          profile.stats?.leadership,
+          profile.stats?.professional,
+          profile.stats?.teamwork,
+        ],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+        strokeWidth: 2, // optional
+      },
+    ],
+    // legend: ["Rainy Days"], // optional
+  };
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
   return (
     <>
       {MainStyles && (
         <View style={[MainStyles.bg, MainStyles.container]}>
-          <Text style={MainStyles.textPrimary}>This is Profile Page</Text>
+          <Text style={MainStyles.textHeader}>{profile.name}</Text>
           {/* <View style={MainStyles.btnAction}><Button title="Challenge" onPress={() => {}}/></View>
            */}
-          <CustomButton type="action" text="Challenge" />
-          <CustomButton
-            text="Submit"
-            divStyle={{
-              backgroundColor: "#BB86FC",
-              borderRadius: 10,
-              padding: remToDp(2),
-              width: "auto",
-            }}
-            textStyle={{ color: "#FFFFFF", fontSize: 18 }}
-          />
+          <View style={{ flexDirection: "row" }}>
+            <Image
+              source={require("../assets/images/epic_avatar.png")}
+              style={{ width: 200, height: 300 }}
+              resizeMode="contain"
+            />
+            <View
+              style={{
+                justifyContent: "flex-end",
+                alignItems: "center",
+                marginBottom: 10,
+              }}
+            >
+              <View style={styles.iconsContainer}>
+                <Icon
+                  name="facebook"
+                  size={30}
+                  color="#3b5998"
+                  style={styles.icon}
+                />
+                <Icon
+                  name="linkedin-square"
+                  size={30}
+                  color="#0077b5"
+                  style={styles.icon}
+                />
+                <Icon
+                  name="twitter"
+                  size={30}
+                  color="#1da1f2"
+                  style={styles.icon}
+                />
+              </View>
+              <LineChart
+                data={data}
+                width={windowWidth / 3}
+                height={100}
+                chartConfig={chartConfig}
+              />
+            </View>
+          </View>
+          <Text style={[MainStyles.textHeader, { fontSize: 20 }]}>
+            Level {profile.level}
+          </Text>
+          <View style={{ marginTop: 5, position: "relative" }}>
+            <Progress.Bar
+              progress={0.6}
+              width={barWidth}
+              height={20}
+              color={MainStyles.colors.bargraphColor}
+            />
+            <Text
+              style={[
+                MainStyles.textHeader,
+                {
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  textAlign: "center",
+                  fontSize: 15,
+                  color: MainStyles.colors.bargraphTextColor,
+                },
+              ]}
+            >
+              {457} exp to Level {10}
+            </Text>
+          </View>
+          <Text style={[MainStyles.textHeader, { fontSize: 18 }]}>
+            Average performances over 7 projects
+          </Text>
+          <View style={styles.container}>
+            <View style={styles.row}>
+              <View style={styles.cell}>
+                <ProfileStatsCard
+                  title={"Professional"}
+                  score={8.2}
+                  imageSource={require("../assets/icons/professional.png")}
+                />
+              </View>
+              <View style={styles.cell}>
+                <ProfileStatsCard
+                  // title={profile.stats?.teamwork}
+                  title={"Teamwork"}
+                  score={9.3}
+                  imageSource={require("../assets/icons/teamwork.png")}
+                />
+              </View>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.cell}>
+                <ProfileStatsCard
+                  title={"Leadership"}
+                  score={6.62}
+                  imageSource={require("../assets/icons/leadership.png")}
+                />
+              </View>
+              <View style={styles.cell}>
+                <ProfileStatsCard
+                  title={"Support"}
+                  score={7.21}
+                  imageSource={require("../assets/icons/innovation.png")}
+                />
+              </View>
+            </View>
+          </View>
         </View>
       )}
     </>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    width: windowWidth * 0.9,
+  },
+  cell: {
+    flex: 1,
+    padding: 10,
+    height: windowHeight * 0.1,
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
 export default Profile;
