7bc95f4fdcdf19798830260f60552f3d5c2026ed
speedyfish <brianlimjj5@gmail.com>
2023-06-18 13:55:13 +0800

    added profile pages

--- App.js
+++ App.js
@@ -6,19 +6,18 @@ import Profile from "./pages/profile";
 // import ChallengeStack from "./pages/challenge.js";
 import Home from "./pages/home";
 
-
 import Header from "./components/header";
-import {auth} from './components/firebase'; 
+import { auth } from "./components/firebase";
 
 import { FontAwesome } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as React from "react";
 
-import {Icon} from "./components/icons";
+import { Icon } from "./components/icons";
 import { PixelRatio } from "react-native";
-import {Challenge, Challenge2, Challenge3} from './pages/challenge';
-import LoginScreen from './pages/login';
-
+import { Challenge, Challenge2, Challenge3 } from "./pages/challenge";
+import LoginScreen from "./pages/login";
+import Profilestatistics from "./pages/profilestatistics";
 
 export default function App() {
   // const Tab = createBottomTabNavigator();
@@ -34,16 +33,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="login" component={LoginScreen}/>
+        {/* <Stack.Screen name="login" component={LoginScreen} /> */}
         <Stack.Screen name="HomeTabs" component={TabsNav} />
         <Stack.Screen name="Challenge2" component={Challenge2} />
         <Stack.Screen name="Challenge3" component={Challenge3} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Profilestatistics" component={Profilestatistics} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export function TabsNav(){
+export function TabsNav() {
   const Tab = createBottomTabNavigator();
 
   const remToDp = (rem) => rem * PixelRatio.get();
@@ -54,47 +55,47 @@ export function TabsNav(){
   // }
 
   return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return <Icon 
-            width={25} height={25} color={color} name={route.name}/>
-
-          },
-          tabBarActiveTintColor: "#BB86FC",
-          tabBarInactiveTintColor: "#BDBDBD",
-          tabBarStyle: {
-            backgroundColor: "#272A37", // Set the background color of the tab bar
-            borderTopWidth: 0.5,
-            borderTopColor: "#414657",
-            paddingBottom: remToDp(1) + 15,
-            paddingTop: remToDp(1),
-            paddingHorizontal: 5,
-          },
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          options={{
-            header: () => <Header title="BeyondEmber"/>
-            // The style below is not applying 
-          }}
-          component={Home}
-        />
-        <Tab.Screen
-          options={{
-            header: () => <Header title="Challenge"/>
-          }}
-          name="Challenge"
-          component={Challenge}
-        />
-        <Tab.Screen
-          options={{
-            header: () => <Header title="Profile"/>
-          }}
-          name="Profile"
-          component={Profile}
-        />
-      </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return (
+            <Icon width={25} height={25} color={color} name={route.name} />
+          );
+        },
+        tabBarActiveTintColor: "#BB86FC",
+        tabBarInactiveTintColor: "#BDBDBD",
+        tabBarStyle: {
+          backgroundColor: "#272A37", // Set the background color of the tab bar
+          borderTopWidth: 0.5,
+          borderTopColor: "#414657",
+          paddingBottom: remToDp(1) + 15,
+          paddingTop: remToDp(1),
+          paddingHorizontal: 5,
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        options={{
+          header: () => <Header title="BeyondEmber" />,
+          // The style below is not applying
+        }}
+        component={Home}
+      />
+      <Tab.Screen
+        options={{
+          header: () => <Header title="Challenge" />,
+        }}
+        name="Challenge"
+        component={Challenge}
+      />
+      <Tab.Screen
+        options={{
+          header: () => <Header title="Profile" />,
+        }}
+        name="Profile"
+        component={Profile}
+      />
+    </Tab.Navigator>
   );
 }
